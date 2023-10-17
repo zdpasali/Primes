1590dc15c73154da84113d4498e00085d5982a6c
Julijan Šribar <julijan.sribar@gmail.com>
2023-10-18 00:00:48 +0200

    Test

---
+++ UnitTests/TestGeneratePrimes.cs
@@ -0,0 +1,17 @@
+namespace UnitTests
+{
+    [TestClass]
+    public class TestGeneratePrimes
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.Fail();
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
